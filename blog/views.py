fc186d85720db66a900d62a7922ff9eece18718c
Jessica Fransson <jessicafransson@outlook.com>
2022-10-24 16:47:10 +0000

    added like/unlike

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -20,7 +21,7 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "post_detail.html",
@@ -52,7 +53,7 @@ class PostDetail(View):
         else:
             comment_form = CommentForm()
 
-        
+
         return render(
             request,
             "post_detail.html",
@@ -64,3 +65,15 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
